19e27fa48a8f1efe914eb6adc77fc88e6097fb1e
Andre Motta <andre.motta@dcc.ufmg.br>
2019-09-03 15:41:27 -0400

    Finished HW2

--- hw/2/main.py
+++ hw/2/main.py
@@ -66,19 +66,25 @@ class Tbl:
         self.count = 0
         self.cols = []
         self.rows = []
+        self.fileline = 0
+        self.linesize = 0
+        self.bannedcols = []
         if string:
             lines = self.string(file)
         else:
             lines = self.read(file)
         lines = self.linemaker(lines)
         first = True
-        for line in lines:
-            if first:
-                first = False
-                self.create_cols(line)
-            else:
-                self.insert_row(line)
-        print("Success")
+        with open("outpart1.txt", "w") as f:
+            for line in lines:
+                f.write(str([self.convert(x) for x in line]))
+                f.write("\n")
+                if first:
+                    first = False
+                    self.create_cols(line)
+                else:
+                    self.insert_row(line)
+            print("Table Created Successfully")
 
     @staticmethod
     def read(file):
@@ -122,90 +128,98 @@ class Tbl:
 
     def create_cols(self, line):
         index = 0
+        cols = []
         for val in line:
-            self.cols.append(Col(index, str(val), Num()))
+            if self.convert(val)[0] == "?":
+                self.bannedcols.append(index)
+            else:
+                cols.append(val)
+                self.cols.append(Col(index, str(val), Num()))
             index += 1
+        with open("outpart2.txt", "w") as f:
+            f.write(str(cols)+ "\n")
+        self.linesize = index
+        self.fileline += 1
 
     def insert_row(self, line):
-        self.rows.append(Row(self.count, [self.convert(x) for x in line], [], 0))
-        self.count += 1
+        f = open("outpart2.txt", "a")
+        self.fileline += 1
+        if len(line) < self.linesize:
+            f.write("E > skipping line " + str(self.fileline) + "\n")
+            f.close()
+            return
+        realline = []
+        realindex = 0
         index = 0
         for val in line:
-            self.cols[index].num + self.convert(val)
+            if index not in self.bannedcols:
+                if val == "?":
+                    realline.append(val)
+                    continue
+                self.cols[realindex].num + self.convert(val)
+                realline.append(val)
+                realindex +=1
             index += 1
+        f.write(str([self.convert(x) for x in realline])+ "\n")
+        self.rows.append(Row(self.count, [self.convert(x) for x in realline], [], 0))
+        self.count += 1
+        f.close()
 
-    def dump(self):
-        print("Dump table:")
-        print("t.cols")
+    def dump(self, f):
+        f.write("Dump table:"+"\n")
+        f.write("t.cols"+"\n")
         for col in self.cols:
-            print("|  "+str(col.oid))
-            print("|  |  add: Num1")
-            print("|  |  col: "+str(col.oid))
-            print("|  |  hi: "+str(col.num.hi))
-            print("|  |  lo: "+str(col.num.lo))
-            print("|  |  m2: "+str(col.num.m2))
-            print("|  |  mu: "+str(col.num.mu))
-            print("|  |  n: "+str(col.num.n))
-            print("|  |  oid: "+str(col.oid))
-            print("|  |  sd: "+str(col.num.sd))
-            print("|  |  txt: "+str(col.txt))
-        print("t.oid: " + str(self.oid))
-        print("t.rows")
+            f.write("|  "+str(col.oid)+"\n")
+            f.write("|  |  add: Num1"+"\n")
+            f.write("|  |  col: "+str(col.oid)+"\n")
+            f.write("|  |  hi: "+str(col.num.hi)+"\n")
+            f.write("|  |  lo: "+str(col.num.lo)+"\n")
+            f.write("|  |  m2: "+str(col.num.m2)+"\n")
+            f.write("|  |  mu: "+str(col.num.mu)+"\n")
+            f.write("|  |  n: "+str(col.num.n)+"\n")
+            f.write("|  |  oid: "+str(col.oid)+"\n")
+            f.write("|  |  sd: "+str(col.num.sd)+"\n")
+            f.write("|  |  txt: "+str(col.txt)+"\n")
+        f.write("t.oid: " + str(self.oid)+"\n")
+        f.write("t.rows"+"\n")
         for row in self.rows:
-            print("|  "+str(row.oid))
-            print("|  |  cells")
+            f.write("|  "+str(row.oid)+"\n")
+            f.write("|  |  cells"+"\n")
             index = 0
             for val in row.cells:
-                print("|  |  |  "+str(index)+": "+str(val))
+                f.write("|  |  |  "+str(index)+": "+str(val)+"\n")
                 index += 1
-            print("|  |  cooked")
-            print("|  |  dom:"+str(row.dom))
-            print("|  |  oid:"+str(row.oid))
+            f.write("|  |  cooked"+"\n")
+            f.write("|  |  dom:"+str(row.dom)+"\n")
+            f.write("|  |  oid:"+str(row.oid)+"\n")
 
 
 
 
 
-def NumCache(cachemu, mu, cachesd, sd):
-    cachemu.append(mu)
-    cachesd.append(sd)
 
 
-def NumCompare(cachemu, mu, cachesd, sd, f, i):
-    cursd = cachesd.pop()
-    curmu = cachemu.pop()
-    if math.isclose(cursd,sd,rel_tol=1e-6) and math.isclose(curmu, mu, rel_tol=1e-6):
-        if i == 100:
-            f.write(
-                    "At iteration "+str(i)+" found : SD = %.4f" % sd + " equal to Saved SD = %.4f" % cursd +
-                    "\n" +
-                    "At iteration "+str(i)+" found : MU = %.4f" % mu + " equal to Saved MU = %.4f" % curmu + '\n'
-                    )
-        else:
-            f.write(
-                "At iteration " + str(i) + "  found : SD = %.4f" % sd + " equal to Saved SD = %.4f" % cursd +
-                "\n" +
-                "At iteration " + str(i) + "  found : MU = %.4f" % mu + " equal to Saved MU = %.4f" % curmu + '\n'
-            )
 def main():
-    s = """$cloudCover, $temp, $humid, $wind,  $playHours
-      100,        68,    80,    0,    3   # comments
-      0,          85,    85,    0,    0
-      0,          80,    90,    10,   0
-      60,         83,    86,    0,    4
-      100,        70,    96,    0,    3
-      100,        65,    70,    20,   0
-      70,         64,    65,    15,   5
-      0,          72,    95,    0,    0
-      0,          69,    70,    0,    4
-      80,          75,    80,    0,    3  
-      0,          75,    70,    18,   4
-      60,         72,    90,    10,   4
-      40,         81,    75,    0,    2    
-      100,        71,    91,    15,   0
-      """
+    s="""$cloudCover, $temp, ?$humid, <wind,  $playHours
+  100,        68,    80,    0,    3   # comments
+  0,          85,    85,    0,    0
+
+  0,          80,    90,    10,   0
+  60,         83,    86,    0,    4
+  100,        70,    96,    0,    3
+  100,        65,    70,    20,   0
+  70,         64,    65,    15,   5
+  0,          72,    95,    0,    0
+  0,          69,    70,    0,    4
+  ?,          75,    80,    0,    ?
+  0,          75,    70,    18,   4
+  60,         72,
+  40,         81,    75,    0,    2
+  100,        71,    91,    15,   0
+  """
     tbl = Tbl(0, s, True)
-    tbl.dump()
+    with open("outpart3.txt", "w") as f:
+        tbl.dump(f)
 
 
 if __name__ == '__main__':
