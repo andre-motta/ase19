2d9819f2d2af39c38165e41948e10d910d288c7c
Andre Motta <andre.motta@dcc.ufmg.br>
2019-09-10 14:13:30 -0400

    hw3 missing abdc

---
+++ hw/3/main.py
@@ -0,0 +1,315 @@
+import math
+import re
+from collections import defaultdict
+import random
+
+
+class Sym:
+    n=0
+    most = 0
+    mode = ""
+
+    def __init__(self):
+        self.cnt = defaultdict(int)
+
+    def __add__(self, v):
+        self.n += 1
+        self.cnt[v] += 1
+        tmp = self.cnt[v]
+        if tmp > self.most:
+            self.most = tmp
+            self.mode = v
+        return v
+
+    def syment(self):
+        e = 0
+        for k, v in self.cnt.items():
+            p = v/self.n
+            e -= p*math.log(p)/math.log(2)
+        return e
+
+    def symany(self, without):
+        r = random.randint()
+        for k, v in self.cnt.items():
+            m = self.n - v if without else v
+            r -= m/self.n
+            if r <= 0:
+                return k
+        return k
+
+    def symlike(self,x,prior,m):
+        f = self.cnt[x]
+        return (f + m*prior)/(self.n + m)
+
+
+class Num:
+    n = 0
+    mu = 0
+    m2 = 0
+    sd = 0
+    lo = float('inf')
+    hi = -float('inf')
+
+    def _numSd(self):
+        if self.m2 < 0:
+            return 0
+        if self.n < 2:
+            return 0
+        return math.sqrt(self.m2/(self.n - 1))
+
+    def numNorm(self, x):
+        return (x - self.lo)/(self.hi - self.lo + 10e-32)
+
+    def __sub__(self, v):
+        if self.n < 2:
+            self.sd = 0
+            return v
+        self.n -= 1
+        d = v - self.mu
+        self.mu -= d / self.n
+        self.m2 -= d*(v - self.mu)
+        self.sd = self._numSd()
+        return v
+
+    def __add__(self, v):
+        self.n += 1
+        if v < self.lo:
+            self.lo = v
+        if v > self.hi:
+            self.hi = v
+        d = v - self.mu
+        self.mu += d / self.n
+        self.m2 += d * (v - self.mu)
+        self.sd = self._numSd()
+        return v
+
+
+class Row:
+    def __init__(self, oid, cells, cooked, dom):
+        self.oid, self.cells, self.cooked, self.dom = oid, cells, cooked, dom
+
+
+class Col:
+    def __init__(self, oid, txt, obj, isnum):
+        self.oid, self.txt, self.obj, self.isnum = oid, txt, obj, isnum
+
+    def __add__(self, v):
+        self.obj + v
+
+    def __sub__(self, v):
+        self.obj - v
+
+
+class Tbl:
+    def __init__(self, oid, file, string):
+        self.oid = oid
+        self.count = 0
+        self.cols = []
+        self.rows = []
+        self.fileline = 0
+        self.linesize = 0
+        self.bannedcols = []
+        self.goals = []
+        self.nums = []
+        self.syms = []
+        self.w = defaultdict(int)
+        self.xnums = []
+        self.xs = []
+        self.xsyms = []
+        if string:
+            lines = self.string(file)
+        else:
+            lines = self.read(file)
+        lines = self.linemaker(lines)
+        first = True
+        for line in lines:
+            if first:
+                first = False
+                self.create_cols(line)
+            else:
+                self.insert_row(line)
+        print("Table Created Successfully")
+
+    @staticmethod
+    def read(file):
+        lines = []
+        with open(file) as f:
+            for line in f:
+                lines.append(line)
+        return lines
+
+    @staticmethod
+    def linemaker(src, sep=",", doomed=r'([\n\t\r ]|#.*)'):
+        "convert lines into lists, killing whitespace and comments"
+        for line in src:
+            line = line.strip()
+            line = re.sub(doomed, '', line)
+            if line:
+                yield line.split(sep)
+
+    @staticmethod
+    def string(s):
+        lines = []
+        for line in s.splitlines():
+            lines.append(line)
+        return lines
+
+    @staticmethod
+    def compiler(x):
+        try:
+            int(x)
+            return int
+        except:
+            try:
+                float(x)
+                return float
+            except ValueError:
+                return str
+
+    def convert(self, x):
+        f = self.compiler(x)
+        return f(x)
+
+    def create_cols(self, line):
+        index = 0
+        cols = []
+        for val in line:
+            val = self.convert(val)
+            if val[0] == "?":
+                self.bannedcols.append(index+1)
+            if "$" in val or "<" in val or ">" in val:
+                self.nums.append(index + 1)
+
+                cols.append(''.join(c for c in val if not c in ['$', '?']))
+                self.cols.append(Col(index+1, str(''.join(c for c in val if not c in ['$', '?'])), Num(), True))
+            else:
+                self.syms.append(index + 1)
+                cols.append(''.join(c for c in val if not c in ['$', '?']))
+                self.cols.append(Col(index+1, str(''.join(c for c in val if not c in ['$', '?'])), Sym(), False))
+
+            if "!" in val or "<" in val or ">" in val:
+                self.goals.append(index+1)
+                if "<" in val:
+                    self.w[index+1] = -1
+            if "<" not in val and ">" not in val and "!" not in val:
+                self.xs.append(index + 1)
+                if "$" in val:
+                    self.xnums.append(index + 1)
+                else:
+                    self.xsyms.append(index + 1)
+            index += 1
+        self.linesize = index
+        self.fileline += 1
+
+    def insert_row(self, line):
+        self.fileline += 1
+        if len(line) < self.linesize:
+            return
+        realline = []
+        realindex = 0
+        index = 0
+        for val in line:
+            if index not in self.bannedcols:
+                if val == "?":
+                    realline.append(val)
+                    continue
+                self.cols[realindex].obj + self.convert(val)
+                realline.append(val)
+                realindex +=1
+            else:
+                realindex+=1
+            index += 1
+        self.rows.append(Row(self.count, [self.convert(x) for x in realline], [], 0))
+        self.count += 1
+
+    def dump(self, f):
+        f.write("Dump table:"+"\n")
+        f.write("t.cols"+"\n")
+        for i, col in enumerate(self.cols):
+            if i+1 in self.bannedcols:
+                continue
+            if col.isnum:
+                f.write("|  "+str(col.oid)+"\n")
+                f.write("|  |  add: Num1"+"\n")
+                f.write("|  |  col: "+str(col.oid)+"\n")
+                f.write("|  |  hi: "+str(col.obj.hi)+"\n")
+                f.write("|  |  lo: "+str(col.obj.lo)+"\n")
+                f.write("|  |  m2: "+str(col.obj.m2)+"\n")
+                f.write("|  |  mu: "+str(col.obj.mu)+"\n")
+                f.write("|  |  n: "+str(col.obj.n)+"\n")
+                f.write("|  |  oid: "+str(col.oid)+"\n")
+                f.write("|  |  sd: "+str(col.obj.sd)+"\n")
+                f.write("|  |  txt: "+str(col.txt)+"\n")
+            else:
+                f.write("|  " + str(col.oid) + "\n")
+                f.write("|  |  add: Sym1" + "\n")
+                for k, v in col.obj.cnt.items():
+                    f.write("|  |  |  " + str(k) + ": " + str(v) + "\n")
+                f.write("|  |  col: "+str(col.oid)+"\n")
+                f.write("|  |  mode: "+str(col.obj.mode)+"\n")
+                f.write("|  |  most: "+str(col.obj.most)+"\n")
+                f.write("|  |  n: " + str(col.obj.n) + "\n")
+                f.write("|  |  oid: " + str(col.oid) + "\n")
+                f.write("|  |  txt: " + str(col.txt) + "\n")
+
+        f.write("t.my: "+"\n")
+        f.write("|  class: " + str(len(self.cols))+"\n")
+        f.write("|  goals" + "\n")
+        for v in self.goals:
+            if v not in self.bannedcols:
+                f.write("|  |  " + str(v) + "\n")
+        f.write("|  nums" + "\n")
+        for v in self.nums:
+            if v not in self.bannedcols:
+                f.write("|  |  " + str(v) + "\n")
+        f.write("|  syms" + "\n")
+        for v in self.syms:
+            if v not in self.bannedcols:
+                f.write("|  |  " + str(v) + "\n")
+        f.write("|  w" + "\n")
+        for k, v in self.w.items():
+            if v not in self.bannedcols:
+                f.write("|  |  " + str(k) + ": "+str(v)+"\n")
+        f.write("|  xnums" + "\n")
+        for v in self.xnums:
+            if v not in self.bannedcols:
+                f.write("|  |  " + str(v) + "\n")
+        f.write("|  xs" + "\n")
+        for v in self.xs:
+            if v not in self.bannedcols:
+                f.write("|  |  " + str(v) + "\n")
+        f.write("|  xsyms" + "\n")
+        for v in self.xsyms:
+            if v not in self.bannedcols:
+                f.write("|  |  " + str(v) + "\n")
+
+
+
+
+
+
+
+def main():
+    s = """   outlook, ?$temp,  <humid, wind, !play
+              rainy, 68, 80, FALSE, yes # comments
+              sunny, 85, 85,  FALSE, no
+              sunny, 80, 90, TRUE, no
+              overcast, 83, 86, FALSE, yes
+              rainy, 70, 96, FALSE, yes
+              rainy, 65, 70, TRUE, no
+              overcast, 64, 65, TRUE, yes
+              sunny, 72, 95, FALSE, no
+              sunny, 69, 70, FALSE, yes
+              rainy, 75, 80, FALSE, yes
+              sunny, 75, 70, TRUE, yes
+              overcast, 72, 90, TRUE, yes
+              overcast, 81, 75, FALSE, yes
+              rainy, 71, 91, TRUE, no
+  """
+    tbl = Tbl(0, s, True)
+    with open("outpart3.txt", "w") as f:
+        tbl.dump(f)
+
+
+if __name__ == '__main__':
+    main()
